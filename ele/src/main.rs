628d385853298fed38422889b7cdc485a485897e
Niklas Sombert <niklas@ytvwld.de>
2024-03-23 22:05:20 +0100

    Basic dbus setup

--- ele/src/main.rs
+++ ele/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use zbus::{Connection, Result, proxy};
+
+#[proxy(
+    interface = "de.ytvwld.Ele1",
+    default_service = "de.ytvwld.Ele",
+    default_path = "/de/ytvwld/Ele"
+)]
+trait EleD {
+    async fn spawn(&self, argv: Vec<&str>) -> Result<String>;
+}
+
+#[async_std::main]
+async fn main() -> Result<()> {
+    let connection = Connection::session().await?;
+    let proxy = EleDProxy::new(&connection).await?;
+    let reply = proxy.spawn(vec!["id"]).await?;
+    println!("{reply}");
+
+    Ok(())
 }
--- eled/src/main.rs
+++ eled/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, future::pending};
+use zbus::{connection, interface};
+
+struct EleD {
+}
+
+#[interface(name = "de.ytvwld.Ele1")]
+impl EleD {
+    async fn spawn(&mut self, argv: Vec<&str>) -> String {
+        todo!()
+    }
+}
+
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let eled = EleD {};
+    let _conn = connection::Builder::session()?
+        .name("de.ytvwld.Ele")?
+        .serve_at("/de/ytvwld/Ele", eled)?
+        .build()
+        .await?;
+
+    // wait forever
+    pending::<()>().await;
+
+    Ok(())
 }
